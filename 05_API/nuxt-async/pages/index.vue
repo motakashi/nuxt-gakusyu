4085d22015e4f7c388d0a18d193bc3868c88b8b4
Takashi <takashi-going-sky@hotmail.co.jp>
2020-04-12 16:58:55 +0900

    API処理

---
+++ 05_API/nuxt-async/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{user.id}}: {{user.name}} ({{user.company.name}})
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require("axios")
+let url = "https://jsonplaceholder.typicode.com/users"
+export default {
+  asyncData({ params, error }) {
+    return axios.get(url)
+    .then((res)=>{
+      return { users: res.data }
+    })
+    .catch((e => {
+      error({users: e.response.status, message: "APIにエラーがありました"});
+    }))
+  },
+}
+</script>
+
+<style>
+
+</style>
